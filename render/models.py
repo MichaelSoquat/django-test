0b908f56428acf6ff710c46378af9cdf3964d8a7
MichaelSoquat <michael.soquat@gmx.de>
2022-08-07 09:22:03 +0200

    rendered some models in view

--- render/models.py
+++ render/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Manufacturer(models.Model):
     name= models.CharField(max_length=64)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Cars(models.Model):
     name = models.CharField(max_length=64)
     color = models.CharField(max_length=32)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
 
 
--- render/views.py
+++ render/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Cars
 
 # Create your views here.
+def renderContentView(request):
+    cars = Cars.objects.all()
+    return render(request, 'render.html', {'cars': cars})
